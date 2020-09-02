164ff5f87671b0f3a6ec39cfddf00a03e3183bf9
Cyn'Rim <cyn.rim.web@gmail.com>
2020-09-02 15:47:03 +0200

    premiere sauvegarde

---
+++ app.js
@@ -0,0 +1,20 @@
+const btnMenu = document.querySelector('.btn-rond-menu')
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
+const ligne = document.querySelector('.cont-ligne');
+
+btnMenu.addEventListener('click', () => {
+    
+    ligne.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
+})
+
+if(window.matchMedia('(max-width: 1300px)')) {
+ 
+    allItemNav.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.toggle('menu-visible')
+            ligne.classList.toggle('active');
+        })
+    })
+}
