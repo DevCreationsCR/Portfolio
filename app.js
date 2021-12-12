dba40203cb77974e49de0cec8ca4577967dc4035
Cynthia Dev' Creations <cynthiadevcreations@gmail.com>
2021-12-12 18:12:52 +0100

    mise à jour du 12/12/2021

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const allItemNav = document.querySelectorAll('.nav-menu-item');
 const ligne = document.querySelector('.cont-ligne');
 
 btnMenu.addEventListener('click', () => {
-    
+
     ligne.classList.toggle('active')
     nav.classList.toggle('menu-visible')
 })
 
-if(window.matchMedia('(max-width: 1300px)')) {
- 
+if (window.matchMedia('(max-width: 1300px)')) {
+
     allItemNav.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.toggle('menu-visible')
@@ -22,42 +22,42 @@ if(window.matchMedia('(max-width: 1300px)')) {
 // ANIMATION ACCUEIL
 const txtAnim = document.querySelector('.txt-animation');
 
-let typewriter = new Typewriter(txtAnim,  {
+let typewriter = new Typewriter(txtAnim, {
     loop: false,
     deleteSpeed: 20 //vitesse pour effacer les lettres
 })
 
-typewriter 
-.pauseFor(1800) //le temps de l'animation donc 1.8s
-.changeDelay(20) //vitesse frappe des lettres
-.typeString('Moi c\'est Olivia Remiki')
-.pauseFor(300)
-.typeString('<strong>, Développeuse <span style="color: #004058;">Full-Stack</span></strong> !')
-.pauseFor(1000)
-.deleteChars(13) //pour effacer les caractères précédents 
-.typeString('<span style="color: #00506e;"> Sass-CSS</span> !')
-.pauseFor(1000)
-.deleteChars(10)
-.typeString('<span style="color: #425b8a;"> PhP-Symfony</span> !')
-.pauseFor(1000)
-.deleteChars(13)
-.typeString('<span style="color: #00506e;"> À Venir</span> !')
-.pauseFor(1000)
-.deleteChars(10)
-.typeString('<span style="color: #067790;"> JavaScript </span> !')
-.start()
+typewriter
+    .pauseFor(1800) //le temps de l'animation donc 1.8s
+    .changeDelay(20) //vitesse frappe des lettres
+    .typeString('Moi c\'est Olivia Remiki')
+    .pauseFor(300)
+    .typeString('<strong>, Développeuse <span style="color: #004058;">Web et Mobile</span></strong> !')
+    .pauseFor(1000)
+    .deleteChars(15) //pour effacer les caractères précédents 
+    .typeString('<span style="color: #00506e;"> Full-Stack</span> !')
+    .pauseFor(1000)
+    .deleteChars(12)
+    .typeString('<span style="color: #425b8a;"> JS, PHP, CSS et SASS</span> !')
+    .pauseFor(1000)
+    .deleteChars(22)
+    .typeString('<span style="color: #00506e;"> C# et Xamarin.Forms</span> !')
+    .pauseFor(1000)
+    .deleteChars(21)
+    .typeString('<span style="color: #067790;"> Web et Mobile</span> !')
+    .start()
 
 // ANIMATION CONTACT
 const input_fields = document.querySelectorAll('input');
 
-for(let i = 0; i < input_fields.length; i++) {
+for (let i = 0; i < input_fields.length; i++) {
 
     let field = input_fields[i];
 
     field.addEventListener('input', (e) => {
-        if(e.target.value !== ''){
+        if (e.target.value !== '') {
             e.target.parentNode.classList.add('animation')
-        } else if (e.target.value == ''){
+        } else if (e.target.value == '') {
             e.target.parentNode.classList.remove('animation')
         }
     })
@@ -72,14 +72,14 @@ const btnMedias = document.querySelectorAll('.media')
 const btnRondAccueil = document.querySelector('.btn-rond')
 
 
-const TL1 = gsap.timeline({paused: true});
+const TL1 = gsap.timeline({ paused: true });
 
-TL1 
-.to(navbar, {left: '0px', ease: Power3.easeOut, duration: 0.6})
-.from(titre, {y: -50, opacity: 0, ease: Power3.easeOut, duration: 0.4})
-.staggerFrom(btn, 1, {opacity: 0}, 0.2, '-=0.30')
-.staggerFrom(btnMedias, 1, {opacity: 0}, 0.2, '-=0.75')
-.from(btnRondAccueil, {y: -50, opacity:0, ease: Power3.easeOut, duration: 0.4}, '-=1')
+TL1
+    .to(navbar, { left: '0px', ease: Power3.easeOut, duration: 0.6 })
+    .from(titre, { y: -50, opacity: 0, ease: Power3.easeOut, duration: 0.4 })
+    .staggerFrom(btn, 1, { opacity: 0 }, 0.2, '-=0.30')
+    .staggerFrom(btnMedias, 1, { opacity: 0 }, 0.2, '-=0.75')
+    .from(btnRondAccueil, { y: -50, opacity: 0, ease: Power3.easeOut, duration: 0.4 }, '-=1')
 
 window.addEventListener('load', () => {
     TL1.play();
@@ -95,9 +95,9 @@ const listePres = document.querySelectorAll('.item-liste')
 const tlpres = new TimelineMax();
 
 tlpres
-.from(titrePres, {y: -200, opacity: 0, duration: 0.6})
-.from(presGauche, {y:-20, opacity: 0, duration: 0.6}, '-=0.5')
-.staggerFrom(listePres, 1, {opacity: 0}, 0.2, '-=0.5')
+    .from(titrePres, { y: -200, opacity: 0, duration: 0.6 })
+    .from(presGauche, { y: -20, opacity: 0, duration: 0.6 }, '-=0.5')
+    .staggerFrom(listePres, 1, { opacity: 0 }, 0.2, '-=0.5')
 
 const controller = new ScrollMagic.Controller();
 
@@ -106,9 +106,9 @@ const scene = new ScrollMagic.Scene({
     triggerHook: 0.5,
     reverse: false
 })
-.setTween(tlpres)
-// .addIndicators()
-.addTo(controller)
+    .setTween(tlpres)
+    // .addIndicators()
+    .addTo(controller)
 
 // ANIMATION PORTFOLIO
 const portfolioContainer = document.querySelector('.portfolio')
@@ -118,17 +118,17 @@ const itemPortfolio = document.querySelectorAll('.vague1')
 const tlPortfolio = new TimelineMax();
 
 tlPortfolio
-.from(titrePortfolio, {y: -50, opacity: 0, duration: 0.5})
-.staggerFrom(itemPortfolio, 1, {opacity: 0}, 0.2, '-=0.5')
+    .from(titrePortfolio, { y: -50, opacity: 0, duration: 0.5 })
+    .staggerFrom(itemPortfolio, 1, { opacity: 0 }, 0.2, '-=0.5')
 
 const scene2 = new ScrollMagic.Scene({
     triggerElement: portfolioContainer,
     triggerHook: 0.5,
     reverse: false
 })
-.setTween(tlPortfolio)
-// .addIndicators()
-.addTo(controller)
+    .setTween(tlPortfolio)
+    // .addIndicators()
+    .addTo(controller)
 
 
 //VAGUE 2
@@ -137,16 +137,16 @@ const itemPortfolio2 = document.querySelectorAll('.vague2')
 const tlPortfolio2 = new TimelineMax();
 
 tlPortfolio2
-.staggerFrom(itemPortfolio2, 1, {opacity: 0}, 0.2, '-=0.5')
+    .staggerFrom(itemPortfolio2, 1, { opacity: 0 }, 0.2, '-=0.5')
 
 const scene3 = new ScrollMagic.Scene({
     triggerElement: itemPortfolio,
     triggerHook: 0.2,
     reverse: false
 })
-.setTween(tlPortfolio2)
-// .addIndicators()
-.addTo(controller)
+    .setTween(tlPortfolio2)
+    // .addIndicators()
+    .addTo(controller)
 
 
 //VAGUE3
@@ -155,16 +155,16 @@ const itemPortfolio3 = document.querySelectorAll('.vague3')
 const tlPortfolio3 = new TimelineMax();
 
 tlPortfolio3
-.staggerFrom(itemPortfolio3, 1, {opacity: 0}, 0.2, '-=0.5')
+    .staggerFrom(itemPortfolio3, 1, { opacity: 0 }, 0.2, '-=0.5')
 
 const scene4 = new ScrollMagic.Scene({
     triggerElement: itemPortfolio2,
     triggerHook: 0.2,
     reverse: false
 })
-.setTween(tlPortfolio3)
-// .addIndicators()
-.addTo(controller)
+    .setTween(tlPortfolio3)
+    // .addIndicators()
+    .addTo(controller)
 
 
 //ANIMATION RANGE
@@ -178,15 +178,15 @@ const allShadowBarres = document.querySelectorAll('.barre-grises')
 const tlCompetences = new TimelineMax();
 
 tlCompetences
-.from(titreComp, {opacity: 0, duration: 0.6})
-.staggerFrom(allLabel, 0.5, {y: -50, opacity:0}, 0.1, '-=0.5')
-.staggerFrom(allPourcent, 0.5, {y: -10, opacity:0}, 0.1, '-=1')
-.staggerFrom(allShadowBarres, 0.5, {y: -10, opacity:0}, 0.1, '-=1')
-.staggerFrom(allBarres, 0.5, {y: -10, opacity:0}, 0.1, '-=1')
+    .from(titreComp, { opacity: 0, duration: 0.6 })
+    .staggerFrom(allLabel, 0.5, { y: -50, opacity: 0 }, 0.1, '-=0.5')
+    .staggerFrom(allPourcent, 0.5, { y: -10, opacity: 0 }, 0.1, '-=1')
+    .staggerFrom(allShadowBarres, 0.5, { y: -10, opacity: 0 }, 0.1, '-=1')
+    .staggerFrom(allBarres, 0.5, { y: -10, opacity: 0 }, 0.1, '-=1')
 
 const scene5 = new ScrollMagic.Scene({
     triggerElement: sectionComp,
     reverse: false
 })
-.setTween(tlCompetences)
-.addTo(controller);
+    .setTween(tlCompetences)
+    .addTo(controller);
